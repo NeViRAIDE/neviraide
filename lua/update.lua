54ad89fb2ae8113e8db4b450c9fc9cc432e9794f
RAprogramm <andrey.rozanov.vl@gmail.com>
2024-02-05 09:40:53 +0900

    fix: update

--- lua/update.lua
+++ lua/update.lua
@@ -1,20 +1,16 @@
 local function check_for_updates()
-  -- Замените путь на актуальный путь к вашему конфигурационному репозиторию Neovim
   local config_path = '~/.config/nvim'
 
-  -- Выполнение git fetch для получения последних изменений без их применения
   local fetch_cmd = 'cd ' .. config_path .. ' && git fetch'
   os.execute(fetch_cmd)
 
-  -- Проверка наличия обновлений, сравнивая локальную ветку с удаленной
   local handle = io.popen('cd ' .. config_path .. ' && git status -uno')
   local result = handle:read('*a')
   handle:close()
 
-  -- Если есть обновления, выводим уведомление
   if not string.find(result, 'Your branch is up to date') then
-    print(
-      'Обнаружены обновления для конфигурации Neovim. Выполните git pull в ~/.config/nvim для обновления.'
+    vim.notify(
+      'NEVIRAIDE updates detected. Run `git pull` in ~/.config/nvim for updating.'
     )
   end
 end
